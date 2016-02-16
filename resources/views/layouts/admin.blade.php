29c0027e95bed372cdcdd420d685a3063046f7dd
Miguel Antonio Carmona <miguelcar18@gmail.com>
2016-02-15 22:33:43 -0430

    Permisos de escritura para todos los archivos para verlos en linux. Ver nombre del usuario autenticado en admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -33,7 +33,7 @@
             <ul class="nav navbar-top-links navbar-right">
                  <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        
+                        <span>{!! Auth::user()->name !!}</span>
                         <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-user">
