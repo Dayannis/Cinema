05fb6dd8dc59a81451fc7c88874a34ac42ebd691
Dayannis <dayannis.y@gmail.com>
2016-02-24 16:51:45 -0430

    Correcciones

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -7,25 +7,22 @@
 					<a href="index.html"><img src="{{ asset('images/logo.png') }}" alt="" /></a>
 					<p>Movie Theater</p>
 				</div>
-				<div class="search v-search">
-					<form>
-						<input type="text" value="Search.." onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search..';}"/>
-						<input type="submit" value="">
-					</form>
-				</div>
-				<div class="clearfix"></div>
+			<div class="clearfix"></div>
 			</div>
-			<!---contact-->
-	<div class="main-contact">
-			 <h3 class="head">CONTACT</h3>
-			 <p>WE'RE ALWAYS HERE TO HELP YOU</p>
-			 <div class="contact-form">
-				 {!!Form::open(['url'=>'password/email'])!!}
-						 	<div class="col-md-6 contact-left">
-						 		{!!Form::text('email')!!}
-							</div>
-							{!!Form::submit('Enviar Link')!!}
-						 {!!Form::close()!!}
-		     </div>
-	</div>
+
+			<div class="main-contact">
+				 <h3 class="head">CONTACT</h3>
+				 <p>WE'RE ALWAYS HERE TO HELP YOU</p>
+				 <div class="contact-form">
+					 {!!Form::open(['url' => '/password/email'])!!}
+					 	<div class="col-md-6 contact-left">
+					 		
+					 		{!!Form::text('email')!!}
+						</div>
+						
+						{!!Form::submit('Enviar link')!!}
+					 {!!Form::close()!!}
+				</div>
+			</div>
+		</div>
 	@endsection
